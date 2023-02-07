1d3c24c4e64d0fc7e3fc6ed398e7f902751c5450
Tristan FLOCH <tristan.floch@epita.fr>
2023-02-07 12:20:09 +0100

    nix: add package output to flake

--- flake.nix
+++ flake.nix
@@ -8,6 +8,16 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
+      packages.${system} = rec {
+        eww-ws = pkgs.buildGoModule {
+          name = "eww-ws";
+          src = ./.;
+          vendorSha256 = "sha256-Ds78icxEE5DRlNJx8//ME5t3hP/FZQAHA4ZjVMK9h9Y=";
+        };
+
+        default = eww-ws;
+      };
+
       devShells.${system} = {
         default = pkgs.mkShell { buildInputs = with pkgs; [ go gopls ]; };
       };
