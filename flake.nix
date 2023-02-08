aceae18bb87e6f2410c2e648ead1e5f01a3e9bc8
Tristan FLOCH <tristan.floch@epita.fr>
2023-02-08 12:38:26 +0100

    nix: add program to shell

--- flake.nix
+++ flake.nix
@@ -7,19 +7,21 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-    in {
-      packages.${system} = rec {
+    in rec {
+      packages.${system} = {
         eww-ws = pkgs.buildGoModule {
           name = "eww-ws";
           src = ./.;
           vendorSha256 = "sha256-Ds78icxEE5DRlNJx8//ME5t3hP/FZQAHA4ZjVMK9h9Y=";
         };
 
-        default = eww-ws;
+        default = packages.${system}.eww-ws;
       };
 
       devShells.${system} = {
-        default = pkgs.mkShell { buildInputs = with pkgs; [ go gopls ]; };
+        default = pkgs.mkShell {
+          buildInputs = [ pkgs.go pkgs.gopls packages.${system}.eww-ws ];
+        };
       };
     };
 }
